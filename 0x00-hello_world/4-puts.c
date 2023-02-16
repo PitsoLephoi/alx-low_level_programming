b7e155f6c533c7b8ea22442861aa110067269cea
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-02-16 12:57:31 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a line using the puts fuction
+ * Return: 0 (once value retruned, then program is successful) 
+ */
+
+int main(void) 
+
+{ 
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0); 
+
+} 
