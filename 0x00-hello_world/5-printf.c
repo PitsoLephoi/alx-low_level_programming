14cbe6be9c84514ea8bfc354686be52a67dab480
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-02-16 13:16:18 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints a string with proper grammar using printf,
+ *              followed by a new line.
+ *
+ * Return: 0 (Equals Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
