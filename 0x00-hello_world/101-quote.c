a0d941257ed903fd6e588e90f634b883ab55bb97
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-02-16 23:05:40 -0800

    changes to 100-intel and 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (showing an error occurred)
+ * Return: Always 1 (indicating an error occurred)
  */
 int main(void)
 {
 char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, "Error: ", 7);
 write(2, msg, sizeof(msg) - 1);
 return (1);
 }
