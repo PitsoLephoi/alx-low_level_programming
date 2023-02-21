a641afebbfe10f1e079b39cffe1705a3b1946855
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-02-21 14:33:35 -0800

    changes to Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,21 +8,6 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line
- *
- * This function uses the _putchar function to print each character of the
- * lowercase alphabet, starting with 'a' and ending with 'z'. After all
- * the characters are printed, a newline character is printed using _putchar.
- *
- * Return: None (void)
- */
 void print_alphabet(void)
 {
 	char c;
