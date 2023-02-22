89239a9522562a2f494042fb175dac8632f07d66
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-02-21 16:43:30 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,11 @@
+#include <limits.h>
 #include "main.h"
 #include <stdio.h>
 
 /**
  * print_sign - prints the sign of a number
+ *
+ * main - entry point for the program
  *
  * @n: the number to be checked
  *
@@ -27,4 +30,30 @@ int print_sign(int n)
 	putchar('-');
 	return (-1);
 	}
+}
+
+/**
+ * main - entry point for the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_sign(98);
+	putchar('\n');
+	print_sign(0);
+	putchar('\n');
+	print_sign(0xff);
+	putchar('\n');
+	print_sign(-1);
+	putchar('\n');
+	print_sign(99);
+	putchar('\n');
+	print_sign(INT_MAX);
+	putchar('\n');
+	print_sign(INT_MIN);
+	putchar('\n');
+	print_sign(-4);
+	putchar('\n');
+	return (0);
 }
