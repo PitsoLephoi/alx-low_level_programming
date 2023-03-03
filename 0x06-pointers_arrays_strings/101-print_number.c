542e61a71e903c208675412305a01365eff5ac29
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-03-02 17:31:55 -0800

    Task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
- */
+* print_number - prints an integer
+*
+* @n: the integer to print
+*
+* Return: void
+*/
 void print_number(int n)
 {
+	int divisor = 1;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+	_putchar('-');
+	n *= -1;
 	}
 
-	if (n / 10)
-	print_number(n / 10)
-	_putchar(n % 10 + '0');
+	while (n / divisor >= 10)
+	divisor *= 10;
+
+	while (divisor != 0)
+	{
+	_putchar((n / divisor) + '0');
+	n %= divisor;
+	divisor /= 10;
+	}
 }
