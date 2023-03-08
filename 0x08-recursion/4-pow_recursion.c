c9c77026e27d3e89d858649b58a2ffb40d18a022
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-03-08 12:38:38 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value
+ * of x raised to the power of y recursively.
+ * @x: The base number.
+ * @y: The exponent to raise the base number by.
+ *
+ * Return: The result of x raised to the power of y, or -1 if y is negative.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
