c4df4b410e19f9b66497160f99e49a87465db75f
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-03-08 13:16:29 -0800

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_palindrome_helper - helper function to check if a string is palindrome
+ * @s: the string to check
+ * @len: the length of the string
+ * @i: the current index to compare
+ *
+ * Return: 1 if the string is palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int len, int i)
+{
+	if (i >= len / 2)
+		return (1);
+	else if (s[i] != s[len - i - 1])
+		return (0);
+	else
+		return (is_palindrome_helper(s, len, i + 1));
+}
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ * @s: the string to check
+ *
+ * Return: 1 if the string is palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	
+	if (len <= 1)
+		return (1);
+	return (is_palindrome_helper(s, len, 0));
+}
