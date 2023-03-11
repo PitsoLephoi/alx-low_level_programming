eef0791f4a22254f1639fc60df7396300a5f0a1c
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-03-10 19:16:58 -0800

    Final changes to task 5

---
+++ 0x0A-argc_argv/coin.c
@@ -0,0 +1,7 @@
+int get_coins(int cents)
+{
+    int coins = 0;
+    // ...
+    return (coins);
+}
+
---
+++ 0x0A-argc_argv/coin.h
@@ -0,0 +1,7 @@
+#ifndef COIN_H
+#define COIN_H
+
+int get_coins(int cents);
+int main(int argc, char *argv[]);
+
+#endif
