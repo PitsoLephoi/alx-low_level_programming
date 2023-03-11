11b8d126e4c287b0e96f92eea0f284285263e99d
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-03-10 18:23:42 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+ /**
+ *
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv; /* Ignore argv*/
+	printf("%d\n", argc - 1);
+	return (0);
+}
