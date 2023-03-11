8f7ad87d9621c361319c3d765e40987d419d62e6
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2023-03-10 18:52:37 -0800

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 if successful, 1 if an error occurred
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins = get_coins(cents);
+	printf("%d\n", coins);
+
+	return (0);
+}
