1b953be0941df7bbc5ed0f907e103643d3904318
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2024-06-21 03:21:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: program that uses the printf function to print
+ * "with proper grammar, but the outcome is a piece of art".
+ *
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return (0);
+}
+
