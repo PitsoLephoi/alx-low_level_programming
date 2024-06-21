b3e29a9021bf6d4128f81003403aaca849696996
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2024-06-21 03:07:59 -0700

    final updates to 4-puts.c 21st June 2024

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-/*
- * Desctription: This is a program that makes use of the puts function.
- * Return value is 0.
+
+/**
+ * main - Entry point of the program
+ *
+ * Desctription: Prints "Programming is linke building a multilingual puzzle"
+ *               followed by a new line using the puts function.
+ *
+ * Return: Always 0 (success)
  *
  */
 
