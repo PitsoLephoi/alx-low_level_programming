582bea634d1db1a5452724aba2df9be51e19f4ce
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2024-06-21 02:55:56 -0700

    changes to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/*
+ * Desctription: This is a program that makes use of the puts function.
+ * Return value is 0.
+ *
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts(""Programming is like building a multilingual puzzle / n");
 
 	return (0);
 }
