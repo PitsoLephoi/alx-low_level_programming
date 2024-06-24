f05e94e3f23a575cccecf2655dd0ac628a8cfdbc
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2024-06-24 14:00:35 -0700

    update and changes to 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,23 @@
 
 int print_sign(int n)
 {
+    int result;
+
     if(n > 0)
     {
         putchar('+');
-        return(1);
+        result = 1;
     }
     else if (n == 0)
     {
         putchar('0');
-        return(0);
+        result = 0;
     }
-    else
+    else if (n < 0)
     {
         putchar('-');
-        return(-1);
+        result = -1;
     }
-
+    putchar(result + '0');
+    return (result);
 }
