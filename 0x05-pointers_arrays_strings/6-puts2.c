92820e19a5a4392386b07951528a3a8b857e78f3
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2025-04-02 20:10:54 +0200

    6-puts2: Added the puts2 function to print every other character of a string. - The function starts from the first character and prints characters at even indices.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: string to be printed
+ * 
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+    int i = 0;
+    int j = strlen(str);
+
+    while(i < j)
+    {
+        _putchar(str[i]);
+        i += 2;
+    }
+    _putchar('\n');
+}
