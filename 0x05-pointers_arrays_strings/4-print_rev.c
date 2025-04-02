1ebd6e35c1684507b11998574413a80817ef1ecf
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2025-04-02 21:01:27 +0200

    Changes to file 4.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,15 @@
 
  void print_rev(char *s)
  {
-    int i;
-    
+    int i = 0;
+
     while(s[i] != '\0')
+    {
+        ++i;
+    }
+    --i; /*Move i to the last character*/
+    
+    while(i >= 0)
     {
         _putchar(s[i - 1]); /*Print each character starting from the end of the string*/
         --i;
