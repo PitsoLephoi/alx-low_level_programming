f04f39136e79e2db6ed06142bdd1ad7ce5ecfef3
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2025-04-28 17:07:22 +0200

    Tasks 0-3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * 
+ * 
+ */
+
+ unsigned int _strspn(char *s, char *accept)
+ {
+    unsigned int count = 0;
+    int i, j, found;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        found = 0;
+
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found)
+            break;
+
+        count++;
+    }
+
+    return count;
+ 
+}
